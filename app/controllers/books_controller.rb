60d95c4911e2cc8953b95798c3180ed8105644e2
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-02-05 13:23:58 +0000

    [Add] editページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,12 +16,19 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book =Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book =Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
 
 private
 def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show',as:'book'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   root to: "homes#top"
   get 'books' => 'books#index'
+  patch 'books/:id' => 'books#update',as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
